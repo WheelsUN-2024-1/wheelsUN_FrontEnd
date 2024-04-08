bf7224537f303a2cf891ff9cfd187b5fdb841923
Cristian <cquilaguy@unal.edu.co>
2024-04-08 09:31:18 -0500

    New CreditCard Screen finished and main updated with routes

--- lib/add_new_creditCard.dart
+++ lib/add_new_creditCard.dart
@@ -38,11 +38,87 @@ class _AddNewCardScreenState extends State<AddNewCardScreen> {
               cardHolderName: cardHolderName,
               cvvCode: cvvCode,
               showBackView: isCvvFocused,
-              onCreditCardWidgetChange: (CreditCardBrand brand) {},)
+              onCreditCardWidgetChange: (CreditCardBrand brand) {},
+              obscureCardNumber: true,
+              obscureCardCvv: true,),
+            Expanded(
+              child: SingleChildScrollView(
+                child:Column(
+                  children: [
+                    CreditCardForm(
+                      cardNumber: cardNumber,
+                      expiryDate: expiryDate,
+                      cardHolderName: cardHolderName,
+                      cvvCode: cvvCode,
+                      onCreditCardModelChange: onCreditCardModelChange,
+                      formKey: formKey,
+                      inputConfiguration: const InputConfiguration(
+                        cardNumberDecoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Number',
+                          hintText: 'XXXX XXXX XXXX XXXX',
+                        ),
+                        expiryDateDecoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Expired Date',
+                          hintText: 'XX/XX',
+                        ),
+                        cvvCodeDecoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'CVV',
+                          hintText: 'XXX',
+                        ),
+                        cardHolderDecoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Card Holder',
+                        ),
+                      ),
+                      ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        backgroundColor: Color(0xff1b447b),
+                      ),
+                      child: Container(
+                        margin: EdgeInsets.all(8.0),
+                        child: Text(
+                          'Validate',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'halter',
+                            fontSize: 14,
+                            package: 'flutter_credit_card',
+                          ),
+                        ),
+                      ),
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          print('valid!');
+                        } else {
+                          print('invalid!');
+                        }
+                      },
+                      )
+                    
+                  ],
+                )
+              )),
           ],
         ),
       ),
 
     );
+  }
+  void onCreditCardModelChange(CreditCardModel creditCardModel){
+    setState(() {
+     cardNumber=creditCardModel.cardNumber;
+     expiryDate=creditCardModel.expiryDate;
+     cardHolderName=creditCardModel.cardHolderName;
+     cvvCode=creditCardModel.cvvCode;
+     isCvvFocused=creditCardModel.isCvvFocused;
+
+    });
   }
 }
